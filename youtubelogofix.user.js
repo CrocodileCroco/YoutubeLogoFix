e1c1ed6a8a7d1cb160f703d270a56dc6700a5854
CrocodileCroco <CrocodileCroco@users.noreply.github.com>
2018-07-18 13:20:43 +0200

    the script

---
+++ youtubelogofix.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name     No Logo Link
+// @version  1
+// @grant    none
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @match https://www.youtube.com/*
+// ==/UserScript==
+
+
+$("area").attr("href", "https://www.youtube.com/")
